9e8f343d38befaba4b5747d41486d0babc514e25
ele <elearic@aliyun.com>
2020-08-09 22:55:26 +0800

    rabbitmq 生产者示例，消费者示例

---
+++ zzeric-rabbitmq/src/main/java/net/eric/zz/chapter1/RabbitProducer.java
@@ -0,0 +1,61 @@
+package net.eric.zz.chapter1;/**
+ * @Author zz_huns
+ * @Date 2020/8/9 11:42 上午
+ * @Version 1.0
+ */
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ *
+ *  1-1
+ *
+ *  * @author zz_huns  
+ *  @version Id: RabbitProducer.java, v 0.1 2020/8/9 11:42 上午 zz_huns Exp $$
+ *
+ */
+public class RabbitProducer {
+
+    private static final String EXCHANGE_NAME = "exchange_demo";
+    private static final String ROUTING_KEY = "routingkey_demo";
+    private static final String QUEUE_NAME = "queue_demo";
+    private static final String IP_ADDRESS = "127.0.0.1";
+    private static final int PORT = 5672;
+
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(IP_ADDRESS);
+        factory.setPort(PORT);
+        factory.setUsername("root");
+        factory.setPassword("passwd");
+        //创建连接
+        Connection connection = factory.newConnection();
+        //创建信道
+        Channel channel = connection.createChannel();
+        //创建一个持久化、非排他的、非自动删除的交换器
+        channel.exchangeDeclare(EXCHANGE_NAME,"direct",true,false,null);
+        //创建一个持久化、非排他的、非自动删除的队列
+        channel.queueDeclare(QUEUE_NAME,true,false,false,null);
+        //将交换器与对垒通过路由键绑定
+        channel.queueBind(QUEUE_NAME,EXCHANGE_NAME,ROUTING_KEY);
+
+        String message = "hello world";
+
+        //发送一条持久化的消息，hello world
+        channel.basicPublish(EXCHANGE_NAME, ROUTING_KEY,
+                MessageProperties.PERSISTENT_TEXT_PLAIN,
+                message.getBytes());
+
+        //关闭资源
+        channel.close();
+        connection.close();
+
+    }
+}
